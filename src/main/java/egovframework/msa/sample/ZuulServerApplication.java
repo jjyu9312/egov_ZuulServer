f9a34dc44b34ed1388c4d608736b9819571de0da
kkw <byl9312@gmail.com>
2022-04-12 13:57:57 +0900

    API Gateway

--- src/main/java/egovframework/msa/sample/ZuulServerApplication.java
+++ src/main/java/egovframework/msa/sample/ZuulServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableDiscoveryClient
+@EnableZuulProxy // End-Point를 이용한 라우팅을 허용 @EnableZuulServer + PreDecorationFilter + RibbonRoutingFilter + SimpleHostRoutingFilter 기능을 추가로 사용
+@EnableDiscoveryClient // @EnableEurekaClient와 동일한 역할을 수행 다른 점은 Eureka뿐만 아니라 Consul, zookeeper도 지원
 public class ZuulServerApplication {
 
 	// zuul 서버는 API Gateway 역할만 담당
